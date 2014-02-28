017f63f48f1716b369bc26f4cef03c1d6c6ba146
Asaf Berko <asafberko@Asafs-MacBook-Pro.local>
2014-03-01 01:15:23 +0200

    Add new View controller - Add New Record

---
+++ TableViewAelfTest/TableViewAelfTest/AddNewRecord.h
@@ -0,0 +1,13 @@
+//
+//  AddNewRecord.h
+//  TableViewAelfTest
+//
+//  Created by Asaf Berko on 3/1/14.
+//  Copyright (c) 2014 Asaf Berko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddNewRecord : UIViewController
+
+@end
